7de657dfaca460d2221181508d5be96c0fbb41a5
Luis Fernando Florez Bailon <lflorezb@unsa.edu.pe>
2023-05-07 18:55:41 -0500

    Hola Mundo

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,5 +2,6 @@ public class HolaMundo{
 	public static void main(String args[]){
 		System.out.println("¡Hola Mundo!");
 		System.out.println("Bienvenido al curso");
+		System.out.println("Luis Fernando Florez Bailon");
 	}
 }
