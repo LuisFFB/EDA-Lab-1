5bee3c648ab49e9891cd5de645377cb60bd21d3e
Luis Fernando Florez Bailon <lflorezb@unsa.edu.pe>
2023-05-07 20:57:21 -0500

    Java While Loop

---
+++ JavaWhileLoop.java
@@ -0,0 +1,24 @@
+//Syntax
+while (condition) {
+  // code block to be executed
+}
+//Example
+int i = 0;
+while (i < 5) {
+  System.out.println(i);
+  i++;
+}
+
+//The Do/While Loop
+//Syntax
+do {
+  // code block to be executed
+}
+while (condition);
+//Example
+int i = 0;
+do {
+  System.out.println(i);
+  i++;
+}
+while (i < 5);
