5b0c57c5a2b4083e3a6c436d4dddcef09899ee30
Luis Fernando Florez Bailon <lflorezb@unsa.edu.pe>
2023-05-07 21:13:39 -0500

    Java Method Overloading

---
+++ JavaMethodOverloading.java
@@ -0,0 +1,35 @@
+//Example 1
+int myMethod(int x)
+float myMethod(float x)
+double myMethod(double x, double y)
+
+static int plusMethodInt(int x, int y) {
+  return x + y;
+}
+
+//Example 2
+static double plusMethodDouble(double x, double y) {
+  return x + y;
+}
+
+public static void main(String[] args) {
+  int myNum1 = plusMethodInt(8, 5);
+  double myNum2 = plusMethodDouble(4.3, 6.26);
+  System.out.println("int: " + myNum1);
+  System.out.println("double: " + myNum2);
+}
+
+//Example 3
+//static int plusMethod(int x, int y) {
+  return x + y;
+}
+
+static double plusMethod(double x, double y) {
+  return x + y;
+}
+
+public static void main(String[] args) {
+  int myNum1 = plusMethod(8, 5);
+  double myNum2 = plusMethod(4.3, 6.26);
+  System.out.println("int: " + myNum1);
+  System.out.println("double: " + myNum2);
