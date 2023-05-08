aba77ea88b18da0864a03a485f55fe6cc9e3073e
Luis Fernando Florez Bailon <lflorezb@unsa.edu.pe>
2023-05-07 20:18:02 -0500

    Java Syntax

---
+++ JavaSyntax.java
@@ -0,0 +1,11 @@
+//El metodo Main
+//El main()método es obligatorio y lo verá en todos los programas de Java:
+public static void main(String[] args)
+	//main()Se ejecutará cualquier código dentro del método. No se preocupe por las palabras clave antes y después de main. Los conocerás poco a poco mientras lees este tutorial.
+	//Por ahora, recuerde que cada programa Java tiene un classnombre que debe coincidir con el nombre del archivo y que cada programa debe contener el main()método.
+	
+	//System.out.println()
+	//Dentro del main()método, podemos usar el println() método para imprimir una línea de texto en la pantalla:
+public static void main(String[] args) {
+  System.out.println("Hello World");
+}
