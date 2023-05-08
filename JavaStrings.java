145953593fba7ad7c6dc73d4e4eb68bbc0cf573a
Luis Fernando Florez Bailon <lflorezb@unsa.edu.pe>
2023-05-07 20:33:09 -0500

    Java Strings

---
+++ JavaStrings.java
@@ -0,0 +1,11 @@
+String greeting = "Hello";
+//String Length
+String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+System.out.println("The length of the txt string is: " + txt.length());
+//More String Methods
+String txt = "Hello World";
+System.out.println(txt.toUpperCase());   // Outputs "HELLO WORLD"
+System.out.println(txt.toLowerCase());   // Outputs "hello world"
+//Finding a Character in a String
+String txt = "Please locate where 'locate' occurs!";
+System.out.println(txt.indexOf("locate")); // Outputs 7
