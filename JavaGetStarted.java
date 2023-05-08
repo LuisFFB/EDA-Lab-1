e574be465367be7d519c8dd90eab0e7646634e4a
Luis Fernando Florez Bailon <lflorezb@unsa.edu.pe>
2023-05-07 19:49:58 -0500

    Java Get Started

---
+++ JavaGetStarted.java
@@ -0,0 +1,10 @@
+//Configuración para Windows
+//Para instalar Java en Windows:
+
+//1. Vaya a "Propiedades del sistema" (Se puede encontrar en Panel de control > Sistema y seguridad > Sistema > Configuración avanzada del sistema)
+//2. Haga clic en el botón "Variables de entorno" en la pestaña "Avanzado"
+//3. Luego, seleccione la variable "Ruta" en Variables del sistema y haga clic en el botón "Editar"
+//4. Haga clic en el botón "Nuevo" y agregue la ruta donde está instalado Java, seguido de \bin . De forma predeterminada, 
+//Java se instala en C:\Program Files\Java\jdk-11.0.1 (si no se especificó nada más cuando lo instaló). En ese caso, deberá agregar una nueva ruta con: C:\Archivos de programa\Java\jdk-11.0.1\bin
+//Luego, haga clic en "Aceptar" y guarde la configuración
+//6. Por último, abra el símbolo del sistema (cmd.exe) y escriba java -version para ver si Java se está ejecutando en su máquina
