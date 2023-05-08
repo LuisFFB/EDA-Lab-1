54394519c462c3593d30f50c12e3294217df1143
Luis Fernando Florez Bailon <lflorezb@unsa.edu.pe>
2023-05-07 21:07:12 -0500

    Java Methods

---
+++ JavaMethods.java
@@ -0,0 +1,36 @@
+//Create a Method
+public class Main {
+  static void myMethod() {
+    // code to be executed
+  }
+}
+
+//Call a Method
+public class Main {
+  static void myMethod() {
+    System.out.println("I just got executed!");
+  }
+
+  public static void main(String[] args) {
+    myMethod();
+  }
+}
+
+// Outputs "I just got executed!"
+
+//Example
+public class Main {
+  static void myMethod() {
+    System.out.println("I just got executed!");
+  }
+
+  public static void main(String[] args) {
+    myMethod();
+    myMethod();
+    myMethod();
+  }
+}
+
+// I just got executed!
+// I just got executed!
+// I just got executed!
