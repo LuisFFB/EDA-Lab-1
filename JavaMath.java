179f0b369eebff95e8829e69e1322134895f2991
Luis Fernando Florez Bailon <lflorezb@unsa.edu.pe>
2023-05-07 20:47:34 -0500

    Java Math

---
+++ JavaMath.java
@@ -0,0 +1,11 @@
+//Math.max(x,y)
+Math.max(5, 10);
+//Math.min(x,y)
+Math.min(5, 10);
+//Math.sqrt(x)
+Math.sqrt(64);
+//Math.abs(x)
+Math.abs(-4.7);
+//Random Numbers
+Math.random();
+int randomNum = (int)(Math.random() * 101);  // 0 to 100
