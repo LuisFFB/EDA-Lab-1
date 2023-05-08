79dae5a67ed2d69356d006441a5aad2cb29984f8
LuisFFB <64060887+LuisFFB@users.noreply.github.com>
2023-05-07 19:41:48 -0500

    Delete JavaHome.java

--- JavaHome.java
+++
@@ -1,7 +0,0 @@
-//Java es un lenguaje de programación popular. 
-//Java se utiliza para desarrollar aplicaciones móviles, aplicaciones web, aplicaciones de escritorio, juegos y mucho más.
-public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
-}
